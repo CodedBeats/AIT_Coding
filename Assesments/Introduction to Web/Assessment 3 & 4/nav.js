b72acda6c123089ad260231ff5351cecd53df192
Cook <luca.haar@icloud.com>
2022-07-04 15:24:29 +1000

    updated nav code - Introduction to Web - upgraded the toggle side nav bar so its basically complete in style and functionality

--- Assesments/Introduction to Web/Assessment 3 & 4/nav.js
+++ Assesments/Introduction to Web/Assessment 3 & 4/nav.js
@@ -1,21 +1,38 @@
+// DOM elements
+const sideNav = document.getElementById("dom-side-nav")
+const main = document.getElementById("main")
 
+// Toggle Nav function
 let openNav = () => {
-    // phone view adjustments 
-    if (window.screen.width <= 680) {
-        console.log("xx")
-        document.getElementById("mySidenav").style.width = "100%";
-        document.getElementById("main").style.marginLeft = "100%";
-        document.body.style.backgroundColor = "rgba(0,0,0,0.2)";
+    if (sideNav.style.display === "none") {
+        sideNav.style.display = "block"
+        
+        // phone view adjustments 
+        if (window.screen.width <= 680) {
+            console.log("xx")
+            sideNav.style.width = "100%";
+            sideNav.style.marginLeft = "100%";
+            document.body.style.backgroundColor = "rgba(0,0,0,0.2)";
+        } 
+        // desktop view adjustments
+        else {
+            setTimeout(() => { sideNav.style.width = "250px" }, 0);
+            document.body.style.marginLeft = "250px"
+            document.body.style.backgroundColor = "rgba(0,0,0,0.2)";
+        }
     } else {
-        document.getElementById("mySidenav").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
-        document.body.style.backgroundColor = "rgba(0,0,0,0.2)";
+        sideNav.style.width = "0";
+        document.body.style.marginLeft = "0";
+        document.body.style.backgroundColor = "white";
+        setTimeout(() => { sideNav.style.display = "none" }, 500);
     }
 }
 
-
+// Close nav button
 let closeNav = () => {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    sideNav.style.width = "0";
+    setTimeout(() => { sideNav.style.display = "none" }, 500);
+    main.style.marginLeft = "0";
+    document.body.style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
 }
