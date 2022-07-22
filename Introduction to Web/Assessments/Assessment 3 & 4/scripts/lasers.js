1eacfa78652fa3f2c0bb5e32dc6753e161879185
Cook <luca.haar@icloud.com>
2022-07-22 19:59:40 +1000

    laser updates - Introduction to Web Assessment 3&4 - removed line agle, made some magic, cheated with a setTimeout function, lasers now work, but timout seconds and speedX must be simultaneous

--- Introduction to Web/Assessments/Assessment 3 & 4/scripts/lasers.js
+++ Introduction to Web/Assessments/Assessment 3 & 4/scripts/lasers.js
@@ -6,22 +6,28 @@ ctx.canvas.height = window.innerHeight
 let laserArr = []
 
 
-let lineLength = 100
+let lineLength = 0
 let x1 = canvas.width
-let y1 = (Math.random() * canvas.height) - 60
-let speedX = -20
-let speedY = 20
+let x2 = canvas.width - lineLength
+let y1 = (Math.random() * canvas.height)
+// let y1 = 180
+let speedX = -40
+let speedY = 0
 
-const angle = 90
-// Math.floor(Math.random() * 180)
-x2 = x1 + Math.cos((Math.PI / 180.0) * angle - Math.floor(Math.random() * 180)) * lineLength
-y2 = y1 + Math.sin((Math.PI / 180.0) * angle - Math.floor(Math.random() * 180)) * lineLength
+// === angled Lasers ===
+// const angle = 180
+// const r = angle * (Math.PI/180)
+// x2 = x1 + Math.cos(r) * lineLength
+// y2 = y1 + Math.sin(r) * lineLength
+
 
 function draw() {
     ctx.beginPath()
-    ctx.moveTo(x1, y1 );
-    ctx.lineTo(x2, y2 + lineLength);
-    ctx.lineWidth = 8
+    ctx.moveTo(x1, y1)
+    // angled Lasers
+    // ctx.lineTo(x2, y2 + lineLength)
+    ctx.lineTo(x2, y1)
+    ctx.lineWidth = 15
     ctx.lineCap = "round"
     ctx.stroke()
     ctx.shadowColor = "red"
@@ -29,19 +35,20 @@ function draw() {
     ctx.strokeStyle = "rgb(255, 0, 0)"
     ctx.stroke()
 }
+// console.log(`X1 is: ${x1}, Y1 is: ${y1}, X2 is: ${x2}, Y2 is: ${y2}`)
+console.log(`X1 is: ${x1}, Y1 is: ${y1}`)
 
 
 
 function pewpew() {
     x1 += speedX
-    y1 += speedY
 
-    x2 += speedX
-    y2 += speedY
-  
-    // if (x < 0 || x > canvas.height) {
-	//     speed = speed * -1;
-    // }
+    function xSpeed() {x2 += speedX}
+    setTimeout(xSpeed, 50)
+
+    // angled Lasers
+    // x2 += speedX
+    // y2 += speedY
 }
 
 function loopAnimation() {
@@ -58,6 +65,6 @@ window.addEventListener("resize",
     function() {
         canvas.width = innerWidth
         canvas.height = innerHeight
-        init()
+        // init()
     }
 )
