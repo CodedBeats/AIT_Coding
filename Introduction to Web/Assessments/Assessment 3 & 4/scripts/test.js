68efa9fcd63294e3d7695dc694d887bd1bd1d6d5
Luca <luca.haar@icloud.com>
2022-07-29 19:40:34 +1000

    updated styles - Introduction to Web Assessment 3&4 - gave #main a view height of 100% so all pages have footer at bottom, made somew button style updates

--- Introduction to Web/Assessments/Assessment 3 & 4/scripts/test.js
+++ Introduction to Web/Assessments/Assessment 3 & 4/scripts/test.js
@@ -1,7 +1,18 @@
 // === Test Functions === //
 
 
-// Add class function
+// Change text position
+let changePos = () => {
+    let element = document.getElementById("swap")
+    element.style.textAlign = element.style.textAlign === 'end' ? '' : 'end'
+
+    let btn = document.getElementById("swap-btn")
+    if (btn.value == "Testing New Features To Add >") btn.value = "< Testing New Features To Add"
+    else btn.value = "Testing New Features To Add >"
+}
+
+
+// Add class to text
 function addClass() {
     let element = document.getElementById("test1")
     element.classList.toggle("i-added-a-class");
@@ -9,7 +20,7 @@ function addClass() {
 }
 
 
-// Hide div example
+// Hide div 
 const div = document.getElementById("test2")
 
 let visible = () => {
