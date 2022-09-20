4ae47b2b33e64b96177e3cf8c6b72cdf160232a6
Luca <luca.haar@icloud.com>
2022-09-20 14:37:05 +1000

    c++ compile -- can't see from changes made but a lot of C and C++ dependencies were installed to allow for running them in VSCode (very happy this is working)

---
+++ Programming I/random/tests.cpp
@@ -0,0 +1,11 @@
+// Simple C++ program to display "Hello, world!".
+// header file to perform Input/Output operation
+#include <iostream>
+using namespace std;
+
+int main() {
+    // print hello world
+    cout << "Hello, world!"; 
+     // Terminates the main function
+    return 0;               
+}
