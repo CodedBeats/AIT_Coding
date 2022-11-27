e4e6be962ebea5964a7431889248f688ce4868af
Luca <luca.haar@icloud.com>
2022-11-27 19:46:19 +1100

    Task 2 complete -- added comments and pseudocode to task 2 completing it (only task 4 to go)

--- Programming I/Assessment 3 (VS)/Task 2 - Sorter/main.cpp
+++ Programming I/Assessment 3 (VS)/Task 2 - Sorter/main.cpp
@@ -2,7 +2,12 @@
 === PSEUDOCODE ===
 
 START
-    
+    SET intValList[] = random integer values, charValList[] = random char values, doubleValList[] = random double values
+    sort intValList[]
+
+    LOOP (i = 0; i < 10; i++)
+        OUPUT intValList[i]
+    END LOOP
 END
 */
 
@@ -12,14 +17,16 @@ END
 #include <algorithm>
 using namespace std;
 
-// 
 int main() {
-    // 
-    int intX[10] = { 2, 0, 1, 5, 6, 4, 3, -2, -1, -55 };
-    char charX[10] = { 'a', '&', 'A', 'Z', 'K', 'L', '!', '(', '@', 'h'};
-    double doubleX[10] = { 2.55, 77.9, 1.456456, 5, 6, 4.342, 1.3, -2, -1.88, -34 };
+    // declare hard coded array types
+    // ============= Choose any of these to pass into sorter() =============
+    int intValList[10] = { 2, 0, 1, 5, 6, 4, 3, -2, -1, -55 };
+    char charValList[10] = { 'a', '&', 'A', 'Z', 'K', 'L', '!', '(', '@', 'h'};
+    double doubleValList[10] = { 2.55, 77.9, 1.456456, 5, 6, 4.342, 1.3, -2, -1.88, -34 };
 
-    sorter(doubleX);
+    cout << "Sorted Array" << endl;
+    // pass an array into the overloaded function
+    sorter(intValList);
 
     return 0;
 }
--- Programming I/Assessment 3 (VS)/Task 2 - Sorter/sorter.cpp
+++ Programming I/Assessment 3 (VS)/Task 2 - Sorter/sorter.cpp
@@ -2,34 +2,31 @@
 #include <algorithm>
 using namespace std;
 
-
+// integer version of the overloaded func
 void sorter(int intValArr[]) {
-
+    // sort array in ascending order
     sort(intValArr, intValArr + 10);
-
-    cout << "Sorted Array" << endl;
+    // print out array
     for (int i = 0; i < 10; i++) {
         cout << intValArr[i] << "\n";
     }
 }
 
-
+// char version of the overloaded func
 void sorter(char charValArr[]) {
-
+    // sort array in ascending order
     sort(charValArr, charValArr + 10);
-
-    cout << "Sorted Array" << endl;
+    // print out array
     for (int i = 0; i < 10; i++) {
         cout << charValArr[i] << "\n";
     }
 }
 
-
+// double version of the overloaded func
 void sorter(double doubleValArr[]) {
-
+    // sort array in ascending order
     sort(doubleValArr, doubleValArr + 10);
-
-    cout << "Sorted Array" << endl;
+    // print out array
     for (int i = 0; i < 10; i++) {
         cout << doubleValArr[i] << "\n";
     }
