84ad3b62120c949fc7b5e1f33fb28cac54aa1e11
Luca <luca.haar@icloud.com>
2023-03-14 21:02:16 +1100

    updating comments -- changed comments in main file to help plan for future updates

--- Programming II/Assessment 3/Accretion^2/Accretion^2/main.cpp
+++ Programming II/Assessment 3/Accretion^2/Accretion^2/main.cpp
@@ -7,14 +7,14 @@ int main() {
     game();
 
     /*
-        1. Seperate classes.h into player and enemy header files
+        Fixes
+        1. 
         2. implement tabs
 
         Features to add
         1. Different enemys
         2. A boss objective (ask if player wants to fight boss, on defeat they just go back to enemys)
         3. An inv display
-        4. Change battle stats to only be HP, Luck, (others to come)
         5. exp
         6. levels (str increses more than def), could differ depending on class
         7. class system?
