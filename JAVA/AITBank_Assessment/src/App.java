dc8d30023b8e8a27c32efa17b8ddb85be95f8570
Luca <luca.haar@icloud.com>
2023-08-20 04:00:15 +1000

    comment notes -- added notes in app about testing and account PINs

--- JAVA/AITBank_Assessment/src/App.java
+++ JAVA/AITBank_Assessment/src/App.java
@@ -1,11 +1,17 @@
 /*
-*    ===== Project Notes =====
-*    1. The project was coded on the VS Code IDE, 
+*   ===== Project Notes =====
+*   1. The project was coded on the VS Code IDE, 
         I don't beleive there are any changes needed when converting to Netbeans, but please be open to potential difference they have that could cause errors
 *
 *    
-*    ===== ASSUMPTION NOTES =====
-*    1. 
+*   ===== Account Details =====
+*   1. account balance, daily withdraw limit and names are generated randomly
+*   2. Enter different card type from home screen to test different account types
+*   3. Account PINs are as follows:
+    *       Cheque Accout - 7777
+    *       Fixed Accout - 5555
+    *       Net-Saver Accout - 2020
+    *       Savings Accout - 1234
 */
 
 
