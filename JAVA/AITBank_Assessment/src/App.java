c50c25a2df6c7bc9508d7e8c2365b861798cb218
Luca <luca.haar@icloud.com>
2023-08-20 03:54:31 +1000

    comment cleanup

--- JAVA/AITBank_Assessment/src/App.java
+++ JAVA/AITBank_Assessment/src/App.java
@@ -1,6 +1,7 @@
 /*
 *    ===== Project Notes =====
-*    1. The project was coded on the VS Code IDE, I don't beleive there are any changes needed when converting to Netbeans, but please be open to potential difference they have that could cause errors
+*    1. The project was coded on the VS Code IDE, 
+        I don't beleive there are any changes needed when converting to Netbeans, but please be open to potential difference they have that could cause errors
 *
 *    
 *    ===== ASSUMPTION NOTES =====
