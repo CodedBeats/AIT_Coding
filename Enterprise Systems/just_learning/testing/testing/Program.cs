c51776f5cd3ecc1ccd9ec82d78da2355f621aaca
Luca <luca.haar@icloud.com>
2023-10-02 09:01:11 +1100

    seperate files, weird get and set, inheritence -- seperated stuff into different files to make it cleaner (it's weird since you don't have to import classes, the ayy exist within the namespace), c# aparently has it's own special way for getting and setting class attributes so I probably need to start using that (it uses capital letter stuff, could maybe replace with `this.` for familiarity), added an example of inheritence which uses `base()` instead of super but is pretty similar to c++

--- Enterprise Systems/just_learning/testing/testing/Program.cs
+++ Enterprise Systems/just_learning/testing/testing/Program.cs
@@ -1,91 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace dotnetApp
+namespace testing
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"The remainder of 9 / 4 is {getRemainder(9, 4)}");
-            Console.WriteLine("========");
-            getDays();
-            Console.WriteLine("========");
-            collectionsEx();
-            Console.WriteLine("\n========");
-            dictionaryEx();
+            Random r1 = new Random();
 
+            Console.WriteLine($"The remainder of 9 / 4 is {r1.getRemainder(9, 4)}");
+            Console.WriteLine("========");
+            r1.getDays();
+            Console.WriteLine("========");
+            r1.collectionsEx();
+            Console.WriteLine("\n========");
+            r1.dictionaryEx();
+            Console.WriteLine("\n========");
+            r1.createArrFromCol();
+            Console.WriteLine("\n========");
+
+            // Create a Person object
+            Person person = new Person("John Doe", 30);
+            person.DisplayInfo();
+
+            // Create an Employee object
+            Employee employee = new Employee("Jane Smith", 25, 1001, 50000.0);
+            employee.DisplayInfo();
+            employee.DisplayEmployeeInfo();
 
 
             Console.ReadLine();
-        }
-        
-        // operation using parameter example
-        static int getRemainder(int num1, int num2)
-        {
-            int remainder = num1 % num2;
-            return remainder;
-        }
-
-        // print all values in arr
-        static void getDays()
-        {
-            string[] weekDays = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-            for (int i = 0; i < weekDays.Length; i++)
-            {
-                Console.WriteLine($"Today is {weekDays[i]}");
-            }
-        }
-
-        // simple list
-        static void collectionsEx()
-        {
-            // create list
-            List<string> colors = new List<string> { "red", "green", "blue"};
-
-            // Iterate through the list.
-            Console.Write("Initial list:\n");
-            foreach (string color in colors)
-            {
-                Console.Write($"{color}, ");
-            }
-
-            // remove item from list
-            colors.Remove("red");
-            Console.Write("\n\nList after removed item:\n");
-            // Iterate through the list in a different way
-            for (int i = 0; i < colors.Count; i++)
-            {
-                Console.Write($"{colors[i]}, ");
-            }
-
-            // add removed item back
-            colors.Add("red");
-            // Iterate through the list.
-            Console.Write("\n\nList after added item:\n");
-            foreach (string color in colors)
-            {
-                Console.Write($"{color}, ");
-            }
-        }
-
-        // dictionary example
-        static void dictionaryEx()
-        {
-            Dictionary<int, string> dict1 = new Dictionary<int, string>();
-
-            // Adding key/value pairs in the Dictionary
-            dict1.Add(1, "hello");
-            dict1.Add(2, "world");
-            dict1.Add(3, "!");
-
-            foreach (KeyValuePair<int, string> ele1 in dict1)
-            {
-                Console.WriteLine("{0} and {1}", ele1.Key, ele1.Value);
-            }
         }
     }
 }
