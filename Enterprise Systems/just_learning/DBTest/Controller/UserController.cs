d1c5cbb309e7d63320ab4efc439a300fda7da2b8
Luca <luca.haar@icloud.com>
2023-10-17 21:38:51 +1100

    fixed db issue, base setup -- fixed the weird db issue going on by reinstalling visual studio (i lost my themes and extensions :( lol), got a base setup for the db thing for next class (week 5)

---
+++ Enterprise Systems/just_learning/DBTest/Controller/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Controller
+{
+    public class UserController
+    {
+        public void GetAllUsers()
+        {
+            UserLogic userLogic = new UserLogic();
+        }
+    }
+}
---
+++ Enterprise Systems/just_learning/DBTest/Model/UserDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.DataSetUserTableAdapters;
+
+namespace Model
+{
+    public class UserDAO
+    {
+        public void GetAllUsers()
+        {
+            // connects to the database
+            TabUserTableAdapter tabUserTableAdapter = new TabUserTableAdapter();
+            DataSetUser.TabUserDataTable tabUserDataTable = tabUserTableAdapter.GetAllUsers();
+        }
+    }
+}
