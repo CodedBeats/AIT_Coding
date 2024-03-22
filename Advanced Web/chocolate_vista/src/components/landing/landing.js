17ef258d46c538bea080cb6e99dfcd4f3da6bbfa
Luca <luca.haar@icloud.com>
2024-03-23 00:38:31 +1100

    custom img carousel -- created a custom carousel component to pass different images and styles (based on className) and added it to landing page with the intention of also having it in chocolate item page

--- Advanced Web/chocolate_vista/src/components/landing/landing.js
+++ Advanced Web/chocolate_vista/src/components/landing/landing.js
@@ -1,17 +1,26 @@
 // dependencies
-
 import { Link } from "react-router-dom";
 
 // components
+import ImageCarousel from "../common/ImageCarousel";
+
+// style
+import "./css/landing.css";
+import "../common/css/carousel.css";
 
 
 let Landing = () => {
+
+    const carouselImage = ["/imgs/choc1.jpg", "/imgs/choc2.jpg"];
+
     return (
         <div className="landing-page">
-            <Link to="/chocolates">
-                <h4>All Boxed Chocolates</h4>
-            </Link>
-            
+            <div className="carousel-section-container">
+                <ImageCarousel images={carouselImage} imageClass="landing-carousel-image" />
+            </div>
+                <Link to="/chocolates">
+                    <h4>All Boxed Chocolates</h4>
+                </Link>
         </div>
     );
 }
