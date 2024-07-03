d0e0ddcbfc97ff5a1cdcba9810020314eae1d1ec
CodedBeats <luca.haar@icloud.com>
2024-07-03 17:27:52 +1000

    download week 5 proj

--- Cross Platform Apps/Class_Learning/Project1/app/login.tsx
+++ Cross Platform Apps/Class_Learning/Project1/app/login.tsx
@@ -1,18 +1,29 @@
-import { View, Text, StyleSheet } from "react-native";
-import { useContext } from "react"
+import { View, Text, StyleSheet, StatusBar } from 'react-native'
+import { Link } from 'expo-router'
+import { AuthForm } from '@/components/AuthForm'
 
-export default function LoginScreen() {
+export default function Login(props: any) {
     return (
-        <View style={styles.container}>
-            <Text>Login</Text>
+        <View>
+            <AuthForm title="Sign in to your account" actionText="Sign in" />
+            <View style={ styles.container }>
+                <Text>Don't have an account?</Text>
+                <Link href='/'>
+                    <Text style={ styles.link } >Go to Sign up</Text>
+                </Link>
+            </View>
         </View>
-    );
+    )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flexDirection: "row",
         justifyContent: "center",
-        alignItems: "center",
+        marginVertical: 15,
     },
-});
+    link: {
+        color: "#b8111e",
+        fontWeight: 700,
+    }
+})
