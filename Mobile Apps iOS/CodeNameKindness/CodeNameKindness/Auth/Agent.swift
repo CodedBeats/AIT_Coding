9e738696037709eebf02671a6d329c93d1518d02
CodedBeats <luca.haar@icloud.com>
2024-07-26 03:19:47 +1000

    CodeNameKindness project setup, Agent, Friend, extensions -- setup the basse project for the app, created the Agent class, created the Friend class which will be for individual docs in a sub collection of friends in the Agent coll

---
+++ Mobile Apps iOS/CodeNameKindness/CodeNameKindness/Auth/Agent.swift
@@ -0,0 +1,44 @@
+//
+//  Agent.swift
+//  CodeNameKindness
+//
+//  Created by Luca Haar on 26/7/2024.
+//
+
+import Foundation
+import FirebaseFirestore
+
+class Agent {
+    var id: String!
+    var agentName: String
+    var email: String
+    var currentMission: String
+    var level: Int
+    var exp: Int
+    var badges: [String]
+    var friends: [Friend] = [Friend]() // init friends collection to not be nil
+    
+    
+    init(id: String!, agentName: String, email: String, currentMission: String, level: Int, exp: Int, badges: [String]) {
+        self.id = id
+        self.agentName = agentName
+        self.email = email
+        self.currentMission = currentMission
+        self.level = level
+        self.exp = exp
+        self.badges = badges
+    }
+    
+    convenience init(id: String, dictionary: [String: Any]) {
+        self.init(
+                    id: id,
+                    agentName: dictionary["agentName"] as! String,
+                    email: dictionary["email"] as! String,
+                    currentMission: dictionary["currentMission"] as! String,
+                    level: dictionary["level"] as! Int,
+                    exp: dictionary["exp"] as! Int,
+                    badges: dictionary["badges"] as! [String]
+                  )
+    }
+}
+
---
+++ Mobile Apps iOS/CodeNameKindness/CodeNameKindness/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeNameKindness
+//
+//  Created by Luca Haar on 26/7/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
