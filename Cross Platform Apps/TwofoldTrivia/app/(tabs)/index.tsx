9ad2b90cc130f3e4f21b3917d7055a9bad518df5
CodedBeats <luca.haar@icloud.com>
2024-08-01 15:38:37 +1000

    game info and rules -- added game info text and rules onto home page

--- Cross Platform Apps/TwofoldTrivia/app/(tabs)/index.tsx
+++ Cross Platform Apps/TwofoldTrivia/app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 // dependencies
-import { View, Text, StyleSheet, Pressable } from "react-native"
+import { View, Text, StyleSheet, Pressable, FlatList } from "react-native"
 import { useContext } from "react"
 import { useRouter } from "expo-router"
 import { useNavigation } from "expo-router"
@@ -27,11 +27,23 @@ export default function HomeScreen() {
             </View>
 
             <View style={styles.gameInfo}>
-                <Text style={styles.gameInfoTitle}>Rules</Text>
-                <Text>TBD</Text>
-
                 <Text style={styles.gameInfoTitle}>Game Info</Text>
-                <Text>TBD</Text>
+                <Text>
+                    Welcome to Twofold Trivia! Test your knowledge and quick thinking by choosing the correct category for each word. Challenge yourself to score high and climb the leaderboard. Enjoy a fun and educational trivia experience designed for both casual players and trivia enthusiasts!
+                </Text>
+
+                <Text style={styles.gameInfoTitle}>Rules</Text>
+                <FlatList
+                    data={[
+                    {key: "> Objective: Match each word with the correct category as quickly as possible."},
+                    {key: "> You will be given a series of words, each with two possible category options."},
+                    {key: "> Tap the left or right button to choose the category you think is correct."},
+                    {key: "> Time Limit: Each game is timed, so think fast to maximize your score!"},
+                    {key: "> Earn points for each correct answer. Aim for the highest score to top the leaderboard!"},
+                    {key: "> Compare your scores with other players and strive to become the top trivia master!"},
+                    ]}
+                    renderItem={({item}) => <Text>{item.key}</Text>}
+                />
             </View>
         </View>
     )
